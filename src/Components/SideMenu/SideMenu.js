9dcf0409eae2a4ae8c9964bb406af6beace2d5ff
Pierre Royce <proyceus@gmail.com>
2021-07-15 15:13:06 -0400

    Changed menu from side to center for better UI

--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -4,14 +4,12 @@ import "./SideMenu.css";
 
 const SideMenu = ({ isLoggedIn, onClick }) => {
   return (
-    <div className="side-menu">
-      <MenuList>
+      <MenuList className="horizontal-menu">
         <MenuItem>Home</MenuItem>
         <MenuItem>My Profile</MenuItem>
         <MenuItem>Favorites</MenuItem>
         <MenuItem onClick={onClick}>{isLoggedIn ? "Log out" : "Log in"}</MenuItem>
       </MenuList>
-    </div>
   );
 };
 
