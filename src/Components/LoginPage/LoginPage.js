ec06fbe982dcd557a53fc56481cfff315908ee9b
Pierre Royce <proyceus@gmail.com>
2021-07-26 17:38:49 -0400

    Inputted form to use for login pages

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import './LoginPage.css';
 
 const LoginPage = () => {
   return (
-    <div>
-      <Typography variant="h1">Login here</Typography>
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form>
+      <label>
+        <p>Username</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Password</p>
+        <input type="password" />
+      </label>
+      <div>
+        <Button type="submit">Submit</Button>
+      </div>
+    </form>
     </div>
   )
 }
